ed92f3ee70acfcb1967dfb46562a889f2d71c26a
gilwamae <gilwayne900@gmail.com>
2026-09-07 18:44:51 +0300

    zwaraarts

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* ==========================================
+   MAIN JAVASCRIPT FILE
+   ========================================== */
+
+/*
+   We will add website interactions here.
+
+   Examples we will add later:
+
+   - Mobile menu
+   - Smooth scrolling
+   - Campaign filters
+   - Donation interactions
+   - Accessibility menu
+   - Text-to-speech
+   - Speech-to-text
+   - Modals
+   - Admin interactions
+*/
+
+
+console.log("Website JavaScript loaded successfully.");
